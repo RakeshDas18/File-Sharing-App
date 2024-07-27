13f9ac18fd38765614b8023055573137995c1604
Rakesh Das <157940757+RakeshDas18@users.noreply.github.com>
2024-07-27 17:05:45 +0530

    Update page.jsx

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.jsx
@@ -52,7 +52,7 @@ export default function Page() {
                             </a>
 
                             <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl dark:text-white">
-                                Welcome to File Sharing App                            
+                                Welcome to Secure Share                            
                             </h1>
 
                             <p className="mt-4 leading-relaxed text-gray-500 dark:text-gray-400 p-11">
